7be8e5384718770fdf8d7b350ce2959c5bdec987
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2024-11-05 23:34:32 -0600

    Update actividad_integradora_TI3002C.py

--- actividad_integradora_TI3002C.py
+++ actividad_integradora_TI3002C.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
+df = pd.read_csv("Police.csv")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
